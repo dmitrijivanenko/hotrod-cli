8f263e753fff7d9883cc1e19e73699a5f2ec41ed
Dmitriy Ivanenko <ivanenko.dmitrij@gmail.com>
2018-05-23 10:58:32 +0300

    typo fix

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
-require 'vendor/autoload.php';
+if (file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+} else {
+    require '../../autoload.php';
+}
 require 'bootstrap-api.php';
