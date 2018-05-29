53d1aaa67889840d5ad52f7dc3bb55de6f8f73ba
Dmitriy Ivanenko <ivanenko.dmitrij@gmail.com>
2018-05-29 07:51:12 +0300

    option for adding the mage-init

--- tests/Jobs/Js/AddMageInitTest.php
+++ tests/Jobs/Js/AddMageInitTest.php
@@ -6,34 +6,25 @@ use HotRodCli\AppContainer;
 use \PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
 
-function file_get_contents($file)
-{
-    return 'var config = {
-            "*": {}
-        };';
-}
-
-function file_put_contents($file, $content)
-{
-    return true;
-}
-
-class AddJsTest extends TestCase
+class AddMageInitTest extends TestCase
 {
     /**
      * @test
      */
-    public function it_adds_js_map_to_requirejs_config_file()
+    public function it_adds_mage_init()
     {
         $appContainer = $this->prophesize(AppContainer::class);
         $fileSystem = $this->prophesize(Filesystem::class);
         $appContainer->resolve()->shouldBeCalled()->withArguments([Filesystem::class])->willReturn($fileSystem->reveal());
 
 
-        $job = new AddJs($appContainer->reveal());
+        $job = new AddMageInit($appContainer->reveal());
 
         $fileSystem->exists()->withArguments(['filename'])->shouldBeCalled()->willReturn(true);
-        $job->handle('filename', 'jsname', 'jsfile');
+        $job->handle('filename', [
+            'name' => 'filename',
+            'bind' => '*'
+        ]);
     }
 
     /**
@@ -46,10 +37,13 @@ class AddJsTest extends TestCase
         $appContainer->resolve()->shouldBeCalled()->withArguments([Filesystem::class])->willReturn($fileSystem->reveal());
 
 
-        $job = new AddJs($appContainer->reveal());
+        $job = new AddMageInit($appContainer->reveal());
 
         $fileSystem->exists()->withArguments(['filename'])->shouldBeCalled()->willReturn(false);
         $this->expectExceptionMessage('no such file');
-        $job->handle('filename', 'jsname', 'jsfile');
+        $job->handle('filename', [
+            'name' => 'filename',
+            'bind' => '*'
+        ]);
     }
 }
